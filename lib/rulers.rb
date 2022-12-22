d5c162e33d0daee366dc597057795641db19cb57
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-22 12:59:06 -0300

    feat: require array.rb file

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require_relative "rulers/array"
 
 module Rulers
   class Application
     def call(env)
       `echo debug > debug.txt`;
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Rulers!"]]
     end
   end
 end
