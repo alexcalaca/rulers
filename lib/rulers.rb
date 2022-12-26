df50df595f29eb65af62eb20c1da31bf28fce212
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-26 11:15:03 -0300

    feat: use routing workflow

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,12 +2,26 @@
 
 require_relative "rulers/version"
 require_relative "rulers/array"
+require_relative "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      klass, act = get_controller_action(env)
+      controller = Klass.new(env)
+      text = controller.send(act)    
+
       [200, {'Content-Type' => 'text/html'}, ["Hello from Rulers!"]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
